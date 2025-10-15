68ef84ebbb21f84ad0892d1a5601bafd98719513
bb3696 <50304309+bb3696@users.noreply.github.com>
2025-10-15 00:39:25 -0500

    add-avatar

--- src/components/AvatarCard.jsx
+++ src/components/AvatarCard.jsx
@@ -1,13 +1,14 @@
 // src/components/AvatarCard.jsx
 import React from 'react';
 import '../css/AvatarCard.css'; // 引入样式文件
+import avatar from "../assets/avatar.jpeg"; // 引入头像图片
 
 function AvatarCard() {
   return (
     <div className="photo-section">
       <img
         className="avatar-img"
-        src="https://media.licdn.com/dms/image/v2/D4D03AQGHgjh7LBpD5w/profile-displayphoto-shrink_400_400/B4DZY9dRs6GwAk-/0/1744787807337?e=1756339200&v=beta&t=6fVsamGCSYec_I-sQnpb7FZLwqngtS_Fce01LcNd2f8"
+        src={avatar}
         alt="Tony Yang"
       />
       <div className="photo-name">Tony Yang</div>
